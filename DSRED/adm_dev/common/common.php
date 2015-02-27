7abc95bc681630597336874aa3c810b4e568f0ab
Dragonslam <dragonslam@gmai.com>
2015-02-27 17:59:17 +0900

    .

--- DSRED/adm_dev/common/common.php
+++ DSRED/adm_dev/common/common.php
@@ -1,19 +1,22 @@
 <?php
+date_default_timezone_set('Asia/Seoul');
+
 // 시스템 관련 
 $sys_doc_root	= $_SERVER['DOCUMENT_ROOT'];
-$sys_doc_path= $_SERVER['SCRIPT_FILENAME'];
+$sys_doc_path	= $_SERVER['SCRIPT_FILENAME'];
 
 // 사이트 관련
 $site_isDebug	= false;
 $site_isLogin	= ($_COOKIE['isLogin'] == "") ? false : $_COOKIE['isLogin'];
 $site_UID		= ($_COOKIE['usr_num'] == "") ? "" : $_COOKIE['usr_num'];
-$site_UMAIL	= ($_COOKIE['user_mail'] == "") ? false : $_COOKIE['user_mail'];
+$site_UMAIL		= ($_COOKIE['user_mail'] == "") ? false : $_COOKIE['user_mail'];
 $site_domain	= $_SERVER['SERVER_NAME'];
-$site_root		= "/mad_new";
+$site_root		= "/DSRED/adm_dev";
+$site_cdn_path	= "/DSRED/adm_dev";
 $site_resource	= "/resource";
 $site_index		= "index.php";
 $site_message	= "";
-$site_test_id	= "macro0630@gmail.com"; // admin@ideacloud.or.kr, test@m2block.com, yjpark@capharm.co.kr , aaaaaa
+$site_test_id	= "test";
 $site_excel_max_cnt	= "4000";
 
 // 로그인 점검
@@ -22,10 +25,10 @@ if ($site_isLogin == true) {
 }
 
 // 페이지 데이터
-$page_Data	["main"]			= array("m", false, -100, "01.main.php", "MAD");
-$page_Data	["info"]			= array("s", false, -100, "02.mad_1.info.php"			, "MAD?");
-$page_Data	["faq"]			= array("s", false, -100, "02.mad_2.faq.php"			, "MAD - Faq");
-$page_Data	["com"]			= array("s", false, -100, "02.mad_3.com.php"			, "MAD - Company");
+$page_Data	["main"]		= array("m", false, -100, "01.main.php", "MAD");
+$page_Data	["info"]		= array("s", false, -100, "02.mad_1.info.php"		, "MAD?");
+$page_Data	["faq"]			= array("s", false, -100, "02.mad_2.faq.php"		, "MAD - Faq");
+$page_Data	["com"]			= array("s", false, -100, "02.mad_3.com.php"		, "MAD - Company");
 $page_Data	["terms1"]		= array("s", false, -100, "02.mad_4.terms1.php"		, "MAD - 이용약관");
 $page_Data	["terms2"]		= array("s", false, -100, "02.mad_5.terms2.php"		, "MAD - 개인정보 취급방침");
 $page_Data	["tracking2"]	= array("s", true, 350, "03.mad_2.tracking.php"		, "MAD - Tracking Event");	// 이벤트 트레킹 등록
@@ -35,30 +38,24 @@ $page_Data	["statistics1"]	= array("s", true, 506, "04.mad_1.statistics.php"	, "
 $page_Data	["statistics2"]	= array("s", true, 558, "04.mad_2.statistics.php"	, "MAD - Statistics");		// 최근 SNS 트레킹 분석
 $page_Data	["statistics3"]	= array("s", true, 610, "04.mad_3.statistics.php"	, "MAD - Statistics");		// 최근 SNS 유입 분석
 $page_Data	["statistics4"]	= array("s", true, 659, "04.mad_4.statistics.php"	, "MAD - Statistics");		// 월별 데이터 : 전체
-$page_Data	["statistics4a"]	= array("s", true, 659, "04.mad_4.statistics_2.php", "MAD - Statistics");			// 월별 데이터 : SNS
 $page_Data	["statistics5"]	= array("s", true, 709, "04.mad_5.statistics.php"	, "MAD - Statistics");		// 일별 데이터 : 전체
-$page_Data	["statistics5a"]	= array("s", true, 709, "04.mad_5.statistics_2.php", "MAD - Statistics");			// 일별 데이터 : SNS
 $page_Data	["statistics6"]	= array("s", true, 761, "04.mad_6.statistics.php"	, "MAD - Statistics");		// 시간별 데이터
 $page_Data	["statistics7"]	= array("s", true, 812, "04.mad_7.statistics.php"	, "MAD - Statistics");		// SNS 콘텐츠 리스트 - 최근한달
-$page_Data	["statistics7a"]	= array("s", true, 812, "04.mad_7.statistics_2.php"	, "MAD - Statistics");		// SNS 콘텐츠 리스트 - 지난달
 $page_Data	["statistics8"]	= array("s", true, 863, "04.mad_8.statistics.php"	, "MAD - Statistics");		// SNS 공유 리스트 - 최근한달
-$page_Data	["statistics8a"]	= array("s", true, 863, "04.mad_8.statistics_2.php"	, "MAD - Statistics");		// SNS 공유 리스트 - 지난달
 $page_Data	["statistics9"]	= array("s", true, 914, "04.mad_9.statistics.php"	, "MAD - Statistics");		// SNS 유입자 리스트 - 최근한달
-$page_Data	["statistics9a"]	= array("s", true, 914, "04.mad_9.statistics_2.php"	, "MAD - Statistics");		// SNS 유입자 리스트 - 지난달
 
 $page_url		= $_SERVER['REQUEST_URI'];
 $page_file		= $_SERVER['SCRIPT_NAME'];
 $page_reffer	= $_SERVER['HTTP_REFERER'];
-$page_id			= ($_GET['q'] == "") ? "main" : $_GET['q'];
+$page_id		= ($_GET['q'] == "") ? "main" : $_GET['q'];
 $page_type		= $page_Data[$page_id	][0];
 $page_auth		= $page_Data[$page_id	][1];
 $page_marker	= $page_Data[$page_id	][2];
-$page_name	= $page_Data[$page_id	][3];
+$page_name		= $page_Data[$page_id	][3];
 $page_title		= $page_Data[$page_id	][4];
 
-
 $t_domain_type	= ($_GET['t'] == "") ? "" : $_GET['t'];
-$t_domain_id		= ($_GET['d'] == "") ? "" : $_GET['d'];
+$t_domain_id	= ($_GET['d'] == "") ? "" : $_GET['d'];
 
 //echo("<br/>$site_root$site_index");
 //echo("<br/>$site_root$site_resource");
---
+++ DSRED/index.php
@@ -0,0 +1,26 @@
+<?
+    $hostname = exec("/home/bin/hostname");
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densitydpi=medium-dpi">	
+	<meta property="og:image" content="">
+	<meta property="og:title" content="대성부동산">
+	<meta property="og:url" content="">
+	<meta property="og:description" content="">
+	<title>::: 대성부동산 :::</title>
+</head>
+<body>
+<div style="text-align:center;">
+<img src="front/resource/images/main.jpg" alt="대성부동산" usemap="#ds_main_link"/>
+<map name="ds_main_link">
+	<area shape="rect" coords="140,210,480,420" href="http://www.ds-house2.com/main.php" alt="StyleRoom">  
+	<area shape="rect" coords="540,210,880,420" href="http://www.ds-house2.com/info_list.php?categoryi=95" alt="StyleRoom - 분양정보">  
+</map>
+</div>
+</body>
+</html>
